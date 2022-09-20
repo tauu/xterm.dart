d281033e02e780b1e91e7ce7eaebfb5fa4e80b03
Georg Wechslberger <tauu@h2overclock.de>
2022-09-20 04:43:49 +0200

    feat: shortcuts may provide a custom callbacks

--- lib/src/ui/shortcut/actions.dart
+++ lib/src/ui/shortcut/actions.dart
@@ -1,14 +1,14 @@
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:xterm/src/terminal.dart';
 import 'package:xterm/src/ui/controller.dart';
-import 'package:xterm/src/ui/selection_mode.dart';
+import 'package:xterm/src/ui/shortcut/shortcuts.dart';
 
 class TerminalActions extends StatelessWidget {
   const TerminalActions({
     super.key,
     required this.terminal,
     required this.controller,
+    required this.shortcuts,
     required this.child,
   });
 
@@ -16,56 +16,16 @@ class TerminalActions extends StatelessWidget {
 
   final TerminalController controller;
 
+  final List<TerminalShortcut> shortcuts;
+
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return Actions(
-      actions: {
-        PasteTextIntent: CallbackAction<PasteTextIntent>(
-          onInvoke: (intent) async {
-            final data = await Clipboard.getData(Clipboard.kTextPlain);
-            final text = data?.text;
-            if (text != null) {
-              terminal.paste(text);
-              controller.clearSelection();
-            }
-            return null;
-          },
-        ),
-        CopySelectionTextIntent: CallbackAction<CopySelectionTextIntent>(
-          onInvoke: (intent) async {
-            final selection = controller.selection;
-
-            if (selection == null) {
-              return;
-            }
-
-            final text = terminal.buffer.getText(selection);
-
-            await Clipboard.setData(ClipboardData(text: text));
-
-            return null;
-          },
-        ),
-        SelectAllTextIntent: CallbackAction<SelectAllTextIntent>(
-          onInvoke: (intent) {
-            controller.setSelection(
-              terminal.buffer.createAnchor(
-                0,
-                terminal.buffer.height - terminal.viewHeight,
-              ),
-              terminal.buffer.createAnchor(
-                terminal.viewWidth,
-                terminal.buffer.height - 1,
-              ),
-              mode: SelectionMode.line,
-            );
-            return null;
-          },
-        ),
-      },
-      child: child,
+    // Convert the list of shortcuts to a callback map.
+    final actions = Map.fromEntries(
+      shortcuts.map((e) => e.toActionMapEntry(terminal, controller)),
     );
+    return Actions(actions: actions, child: child);
   }
 }
